982e0c6df3f0f66f9ffaf90756ce224c38d1df0e
Katherine Gotovsky <katherine.gotovsky@gmail.com>
2020-06-06 23:55:04 -0400

    added team section

--- functions.js
+++ functions.js
@@ -10,3 +10,33 @@ $(document).ready(function(){
     });
   });
 });
+
+//animate beginning text
+var textWrapper = document.querySelector('.ml3');
+
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+textWrapper.innerHTML = textWrapper.innerHTML + "<span class='letter green'>.</span>";
+
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 10,
+    delay: (el, i) => 100 * (i+1)
+  // }).add({
+  //   targets: '.ml3',
+  //   opacity: 0,
+  //   duration: 1000,
+  //   easing: "easeOutExpo",
+  //   delay: 1000
+  });
+
+
+// Scroll Reveals
+ScrollReveal().reveal('#about', { delay: 200, duration: 2000 });
+ScrollReveal().reveal('#program', { delay: 400, duration: 2000 });
+ScrollReveal().reveal('#roles', { delay: 400, duration: 2000 });
+ScrollReveal().reveal('#join', { delay: 200, duration: 2000 });
+ScrollReveal().reveal('#hiring', { delay: 400, duration: 2000 });
--- navbar.js
+++ navbar.js
@@ -1,16 +1,16 @@
-/* only execute this script when the document is ready */
-$(document).ready(function(){
-  /* function called when you click of the button */
-  $(document.getElementById("nav-button").click(function(){
+// /* only execute this script when the document is ready */
+// $(document).ready(function(){
+//   /* function called when you click of the button */
+//   $(document.getElementById("nav-button").click(function(){
       
-      /* this if else to change the text in the button */
-      if($(document.getElementById("nav-button")).text() == "☰"){
-       $(document.getElementById("nav-button")).text("🞬");
-      }else{
-        $(document.getElementById("nav-button")).text("☰");
-      }
+//       /* this if else to change the text in the button */
+//       if($(document.getElementById("nav-button")).text() == "☰"){
+//        $(document.getElementById("nav-button")).text("🞬");
+//       }else{
+//         $(document.getElementById("nav-button")).text("☰");
+//       }
     
-    /* this function toggle the visibility of our "li" elements */
-    $("li").toggle("slow");
-  });
-});
+//     /* this function toggle the visibility of our "li" elements */
+//     $("li").toggle("slow");
+//   }));
+// });
